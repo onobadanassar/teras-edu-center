1b97b2c4617a5ab4f9e6bf68f2937aacece2ad38
عبادة نصار <on.obada.nassar@gmail.com>
2026-07-24 11:56:13 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// ===== config.js =====
+// إعدادات GitHub - غير هذا الملف فقط عند الحاجة
+
+const GITHUB_CONFIG = {
+  token: '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', // 👈 التوكن الخاص بك
+  owner: 'onobadanassar', // اسم المستخدم
+  repo: 'teras-edu-center', // اسم المستودع
+  file: 'data.json' // اسم ملف البيانات
+};
