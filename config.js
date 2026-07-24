906758864e28d6d36fd07cd5e67e70b6be5872a0
عبادة نصار <on.obada.nassar@gmail.com>
2026-07-24 12:23:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-// ===== config.js =====
-// إعدادات GitHub - غير هذا الملف فقط عند الحاجة
-
 const GITHUB_CONFIG = {
-  token: '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', // 👈 التوكن الخاص بك
-  owner: 'onobadanassar', // اسم المستخدم
-  repo: 'teras-edu-center', // اسم المستودع
-  file: 'data.json' // اسم ملف البيانات
+  token: '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',
+  owner: 'onobadanassar',
+  repo: 'teras-edu-center',
+  file: 'data.json'
 };
